97568512b2d8579e134b85b3c4d781c20761c06e
kngvxx <kiki.puk@op.pl>
2019-04-02 18:37:21 +0200

    Giera

---
+++ Clicker/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clicker
+{
+    
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int PointsCount = 0;
+        public static int PointsAdder = 1;
+
+        private void SetPoints()
+        {
+            PointsLabel.Content = $"Ilość punktów: {PointsCount}";
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickerButton_Click(object sender, RoutedEventArgs e)
+        {
+            PointsCount += PointsAdder;
+            SetPoints();
+        }
+    }
+}
